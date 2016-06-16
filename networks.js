9ea863860ad3eea4bbf43bb46769d3a2487809a6
Grant Orchard <gorchard@vmware.com>
2016-06-16 10:48:26 +1000

    Addressing some syntax issues.
    Signed-off-by: Grant Orchard <gorchard@vmware.com>

--- networks.js
+++ networks.js
@@ -1,15 +1,27 @@
-var networks = ["Please select a Network Zone."];
+//var networks = [];
 //var networkZone = [];
 //var datacenterLocation = [];
-
+function networksForLocation(datacenterLocation) {
+	if (datacenterLocation) {
+		switch (datacenterLocation) {
+			case "Sydney": return sydneyNetworks();
+			case "Melbourne": return melbourneNetworks();
+			default : ["No networks avalable."];
+			break;
+		}	
+	} else {
+		return ["Please select a Network Zone."];
+	}
+}
 
 function sydneyNetworks(networkZone) {
-"use strict";
 	if (networkZone) {
 	 	switch(networkZone) {
  			case "Web": return ["VLAN103 - Web Dev","VLAN104 - Web Prod"];
  			case "Application": return ["VLAN105 - App Dev","VLAN106 - App Prod"];
  			case "Database": return ["VLAN107 - DB Dev","VLAN108 - DB Prod"];
+ 			default : ["No networks avalable."];
+ 			break;
 		}
 	} else {
 		return ["Please select a Network Zone."];
@@ -18,25 +30,14 @@ function sydneyNetworks(networkZone) {
 
 
 function melbourneNetworks(networkZone) {
-"use strict";
 	if (networkZone) {
  		switch(networkZone) {
  			case "Web":  return ["VLAN203 - Web Dev","VLAN204 - Web Prod"];
  			case "Application": return ["VLAN205 - App Dev","VLAN206 - App Prod"];
  			case "Database": return ["VLAN207 - DB Dev","VLAN208 - DB Prod"];	
+ 			default : ["No networks avalable."];
+ 			break;
 		}
-	} else {
-		return ["Please select a Network Zone."];
-	}
-}
-
-function networksForLocation(datacenterLocation) {
-"use strict";
-	if (datacenterLocation) {
-		switch (datacenterLocation) {
-			case "Sydney": return sydneyNetworks();
-			case "Melbourne": return melbourneNetworks();
-		}	
 	} else {
 		return ["Please select a Network Zone."];
 	}
